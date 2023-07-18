550a6e739dcafecf46400b44035436b948a59a3f
SushainGupta <sushaingupta135@gmail.com>
2023-07-18 20:07:59 +0530

    Feat: Resposnive Footer

--- src/components/global/Footer.jsx
+++ src/components/global/Footer.jsx
@@ -4,9 +4,9 @@ import FooterLink from "../FooterLinks";
 function Footer() {
 	return (
 		<>
-			<section className="flex h-max flex-col gap-24 bg-slate-950 px-20 pt-28 pb-12 text-slate-200">
-				<div className="flex">
-					<div className="grid w-[70%] grid-cols-4">
+			<section className="flex flex-col bg-slate-950 px-4 py-12 text-slate-200 sm:px-8 lg:px-20">
+				<div className="flex flex-col md:flex-row">
+					<div className="grid w-full grid-cols-2 gap-8 p-8 max-[500px]:grid-cols-1 sm:grid-cols-2 md:w-3/4 lg:w-1/2 xl:w-3/4 2xl:grid-cols-4">
 						<div className="flex flex-col gap-5">
 							<h2 className="text-xl">Services</h2>
 							<ul className="flex flex-col gap-2">
@@ -45,25 +45,26 @@ function Footer() {
 							</ul>
 						</div>
 					</div>
-					<div className="flex flex-col gap-5">
-						<h2 className="text-xl font-bold">Subcribe to our newsletter</h2>
+
+					<div className="flex w-full flex-col gap-5 md:w-1/2 lg:w-1/2 xl:w-1/4 2xl:w-1/4 p-8">
+						<h2 className="text-xl font-bold">Subscribe to our newsletter</h2>
 						<p>
 							Funding freemium long tail hypotheses first mover advantage assets
 							ownership
 						</p>
-						<div className="flex gap-2">
+						<div className="flex flex-col gap-2 lg:flex-row">
 							<input
 								type="email"
 								className="w-full rounded-lg p-2 text-slate-900 focus:outline-none"
 								placeholder="Your email address…"
 							/>
-							<button className="rounded-lg bg-[#6E00FA] p-3">Subscribe</button>
+							<button className="rounded-lg bg-[#6E00FA] p-2">Subscribe</button>
 						</div>
 					</div>
 				</div>
 
 				<p className="w-full border-t border-gray-500 pt-10 text-center text-gray-500">
-					Design with love © TanahAirStudio 2020. All right reserved
+					Design with love © TanahAirStudio 2020. All rights reserved
 				</p>
 			</section>
 		</>
